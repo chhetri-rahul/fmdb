c396c2f6b2b342d28970a317a0c0a892a0906ef6
chhetri-rahul <r9a3hul@gmail.com>
2021-04-15 16:49:50 +0530

    updated dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/chhetri-rahul/SQLCipher.git", branch:("master"))
+        .package(name: "SQLCipher", url: "https://github.com/chhetri-rahul/SQLCipher.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
