45036aa6594c2f662ac21b52be67b8356af8b987
chhetri-rahul <r9a3hul@gmail.com>
2021-04-15 16:25:33 +0530

    updated package url to refer forked sqlcipher

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/antwork/SQLCipher.git", from: "0.0.4")
+        .package(url: "https://github.com/chhetri-rahul/SQLCipher.git", branch:("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
